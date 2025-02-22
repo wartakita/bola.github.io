e859d25dfc34d147a46b9f769fcdaaecee6decdc
wartakita <161806668+wartakita@users.noreply.github.com>
2025-02-22 22:14:36 +0700

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,19 +1,48 @@
-async function fetchMatchData() {
-    const matchId = document.getElementById("matchId").value;
-    if (!matchId) {
-        alert("Please enter a match ID");
-        return;
-    }
-
-    const url = `https://e761dszb.com/prod-api/match/detail?mid=${matchId}&type=1&pid=6&langtype=en`;
+function _0x34fa(_0x2591ab, _0x43b202) {
+  const _0x12ed11 = _0x1421();
+  return _0x34fa = function (_0x4383c4, _0x23c9e0) {
+    _0x4383c4 = _0x4383c4 - 196;
+    let _0x275c7b = _0x12ed11[_0x4383c4];
+    return _0x275c7b;
+  }, _0x34fa(_0x2591ab, _0x43b202);
+}
+(function (_0x2b0237, _0x1a89c7) {
+  const _0x30323 = _0x34fa, _0x2d3f22 = _0x2b0237();
+  while (true) {
     try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error("Failed to fetch data");
-        }
-        const data = await response.json();
-        document.getElementById("result").innerText = JSON.stringify(data, null, 2);
-    } catch (error) {
-        document.getElementById("result").innerText = "Error: " + error.message;
+      const _0x4cf72a = -parseInt(_0x30323(213)) / 1 + parseInt(_0x30323(202)) / 2 + -parseInt(_0x30323(216)) / 3 * (parseInt(_0x30323(230)) / 4) + -parseInt(_0x30323(210)) / 5 * (-parseInt(_0x30323(206)) / 6) + parseInt(_0x30323(196)) / 7 + -parseInt(_0x30323(222)) / 8 + -parseInt(_0x30323(207)) / 9;
+      if (_0x4cf72a === _0x1a89c7) break; else _0x2d3f22.push(_0x2d3f22.shift());
+    } catch (_0x5e1963) {
+      _0x2d3f22.push(_0x2d3f22.shift());
     }
+  }
+}(_0x1421, 616700));
+function _0x1421() {
+  const _0x1c99b0 = ["oNtZe", "626763zwJVkV", "ById", "LjZUl", " ID", "innerText", "il?mid=", "8727384NoFEWh", "pe=en", "fetch data", "value", "matchId", "Failed to ", "Please ent", "ZTFEl", "4cDirnt", "GcAIS", "GlzxK", "d=6&langty", "OEZdN", "er a match", "5465271gAdGpW", "&type=1&pi", "match/deta", "message", "getElement", "json", "2415776rhaIBr", "https://e7", "vZngO", "bOYpZ", "4848PZyshq", "247788dbDewF", "stringify", "61dszb.com", "4020evYzHY", "/prod-api/", "Error: ", "694197Xrowph", "result"];
+  _0x1421 = function () {
+    return _0x1c99b0;
+  };
+  return _0x1421();
+}
+async function fetchMatchData() {
+  const _0x5af5de = _0x34fa, _0x20adf3 = {bOYpZ: _0x5af5de(226), GcAIS: function (_0xb63e0a, _0x4e5e00) {
+    return _0xb63e0a(_0x4e5e00);
+  }, vZngO: _0x5af5de(228) + _0x5af5de(235) + _0x5af5de(219), LjZUl: function (_0x361b26, _0x1ec07c) {
+    return _0x361b26(_0x1ec07c);
+  }, GlzxK: _0x5af5de(227) + _0x5af5de(224), OEZdN: _0x5af5de(214), oNtZe: function (_0x451a66, _0x1331bf) {
+    return _0x451a66 + _0x1331bf;
+  }, ZTFEl: _0x5af5de(212)}, _0x5e2761 = document[_0x5af5de(200) + _0x5af5de(217)](_0x20adf3[_0x5af5de(205)])[_0x5af5de(225)];
+  if (!_0x5e2761) {
+    _0x20adf3[_0x5af5de(231)](alert, _0x20adf3[_0x5af5de(204)]);
+    return;
+  }
+  const _0x4959bb = _0x5af5de(203) + _0x5af5de(209) + _0x5af5de(211) + _0x5af5de(198) + _0x5af5de(221) + _0x5e2761 + (_0x5af5de(197) + _0x5af5de(233) + _0x5af5de(223));
+  try {
+    const _0xc51ba6 = await _0x20adf3[_0x5af5de(218)](fetch, _0x4959bb);
+    if (!_0xc51ba6.ok) throw new Error(_0x20adf3[_0x5af5de(232)]);
+    const _0x3c9040 = await _0xc51ba6[_0x5af5de(201)]();
+    document[_0x5af5de(200) + _0x5af5de(217)](_0x20adf3[_0x5af5de(234)])[_0x5af5de(220)] = JSON[_0x5af5de(208)](_0x3c9040, null, 2);
+  } catch (_0x595d19) {
+    document[_0x5af5de(200) + _0x5af5de(217)](_0x20adf3[_0x5af5de(234)])[_0x5af5de(220)] = _0x20adf3[_0x5af5de(215)](_0x20adf3[_0x5af5de(229)], _0x595d19[_0x5af5de(199)]);
+  }
 }
