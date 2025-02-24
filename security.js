3b157912c7a73b2574c7514a4d8ac387f7309b37
wartakita <161806668+wartakita@users.noreply.github.com>
2025-02-24 14:23:42 +0700

    Update security.js

--- security.js
+++ security.js
@@ -1,22 +1,3 @@
-// Fungsi untuk memeriksa domain
-function checkDomain() {
-    const allowedDomain = 'https://zonasportlive.blogspot.com/'; // Ganti dengan domain yang diizinkan
-    const referrer = document.referrer;
-
-    if (referrer && !referrer.startsWith(allowedDomain)) {
-        alert('Akses dilarang. Anda tidak diizinkan untuk mengakses halaman ini dari domain ini.');
-        window.location.href = allowedDomain; // Redirect ke domain yang diizinkan
-    }
-}
-
-// Fungsi untuk mencegah embedding dalam iframe
-function preventEmbed() {
-    if (window.top !== window.self) {
-        alert('Akses dilarang. Halaman ini tidak dapat di-embed atau di-iframe.');
-        window.top.location = window.self.location;
-    }
-}
-
 // Fungsi untuk memeriksa apakah halaman sedang di-iframe dan redirect jika ya
 function checkIfIframed() {
     if (window.self !== window.top) {
@@ -24,8 +5,5 @@ function checkIfIframed() {
     }
 }
 
-// Panggil fungsi checkDomain dan preventEmbed saat dokumen selesai dimuat
-document.addEventListener('DOMContentLoaded', () => {
-    checkDomain();
-    preventEmbed();
-});
+// Panggil fungsi checkIfIframed saat dokumen selesai dimuat
+document.addEventListener('DOMContentLoaded', checkIfIframed);
