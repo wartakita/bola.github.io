da8cce3e27d5be60e5893802a2fc195a44e483ff
wartakita <161806668+wartakita@users.noreply.github.com>
2025-03-16 10:08:34 +0700

    Update jadwal.js

--- jadwal.js
+++ jadwal.js
@@ -1,157 +1,125 @@
-let allMatches = [];
-let selectedDate = '';
-
+const _0x377c11 = _0x5a8d;
+(function (_0x233057, _0x1f55fa) {
+  const _0x2f66a1 = _0x5a8d, _0x5197de = _0x233057();
+  while (true) {
+    try {
+      const _0x1afc0d = -parseInt(_0x2f66a1(537)) / 1 * (-parseInt(_0x2f66a1(510)) / 2) + parseInt(_0x2f66a1(444)) / 3 + parseInt(_0x2f66a1(433)) / 4 + parseInt(_0x2f66a1(505)) / 5 * (parseInt(_0x2f66a1(467)) / 6) + parseInt(_0x2f66a1(413)) / 7 * (-parseInt(_0x2f66a1(389)) / 8) + -parseInt(_0x2f66a1(447)) / 9 + -parseInt(_0x2f66a1(423)) / 10;
+      if (_0x1afc0d === _0x1f55fa) break; else _0x5197de.push(_0x5197de.shift());
+    } catch (_0x4cb65e) {
+      _0x5197de.push(_0x5197de.shift());
+    }
+  }
+}(_0x1e02, 257025));
+function _0x1e02() {
+  const _0x1d6288 = ["nasportliv", "competitio", "length", "mTHgQ", "QAoOC", 'lue="">Sem', "result", "pButton", "value", "includes", "tTxhe", '0" class="', "classList", "LGHmS", "addEventLi", "\n<pre>", "DOMContent", "QOVCg", "league", "mpetisi: ", "</p>\n<p>Li", "add", "ent", "nhUXk", "schedule", "match_time", "25192sZrDmw", "IOnmj", "tlive", "TrclP", "Zrovj", "tTcDz", "title", ":00", "Tidak dapa", "</p>\n<p>Ko", "scrollToTo", "></p>\n<hr>", "copyJsonBu", "<option va", "forEach", "clipboard", "OPUeF", "block", "hing data:", "map", "none", "tton", "</pre>\n", "ttps://bit", "161pbgMlV", "copyTextBu", 'e" target=', "start_time", "y/zonaspor", "insertCell", "innerHTML", "replace", "scrollTo", "id-ID", "1036640PtPfRE", "createElem", "</p>\n<p>Wa", " cocok.</p", "then", "&timezone=", "loRdu", "insertRow", "scrollY", "searchBox", "479900CnzJYs", "stener", "\nKompetisi", "i.90min.to", "Informasi ", "2-digit", ".ly/zonasp", "active", "matchInfoC", "remove", "home_team", "177717AtFRbF", "nFilter", "XjtMq", "3591864dVtTFD", "catch", "onclick", "filter", "Loaded", "json", "XweyH", "00000", "rFLyR", "toLocaleTi", '"_blank">h', "Alcmj", "name", "stringify", "vobeo", "writeText", "si</option", "cBWUJ", '<img src="', "ps://bit.l", "126smXsFI", "lKUXX", "FScYm", 'me-2"> ', "nkgWN", "appendChil", "jcEhR", "NdHPg", "\nLink: htt", "scroll", "whRJQ", "div", "t menyalin", "wTFdt", "ontainer", "datePicker", "/bit.ly/zo", "\nTanggal: ", "split", "loading", "ivQhj", '" width="2', "join", "toLowerCas", "smooth", "away_team", "logo", "textConten", "\nWaktu: ", "GLFWT", "nggal: ", "display", "</p>\n<p>Ta", "tbody", "meString", "/v1/match/", "msDYa", "sByTagName", "83220yIbrFK", "da pertand", " teks: ", "style", "fFsti", "606948SLJhCJ", "error", "option", "Asia/Jakar", "ktu: ", "\n<p>", "kwtNY", 'f="https:/', "Xgktf", "qYrGT", "erGhr", "ortlive</a", "ua Kompeti", "Error fetc", "vdqAs", "ingan yang", "list?date=", "https://ap", "ById", "nk: <a hre", "EjWLN", "zfPEN", "stPRl", "CBZbu", "push", "finally", "<p>Tidak a", "1QLAVjM", " vs ", "telah disa", "lin!", "iFFgg", "getElement"];
+  _0x1e02 = function () {
+    return _0x1d6288;
+  };
+  return _0x1e02();
+}
+let allMatches = [], selectedDate = "";
 function fetchMatches() {
-    const date = document.getElementById('datePicker').value;
-    selectedDate = date; // Menyimpan tanggal yang dipilih
-    const apiUrl = `https://api.90min.to/v1/match/list?date=${date}&timezone=Asia/Jakarta`;
-    document.getElementById('loading').classList.add('active');
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            allMatches = data.result;
-            populateCompetitionFilter();
-            displayMatches(allMatches);
-        })
-        .catch(error => console.error('Error fetching data:', error))
-        .finally(() => {
-            document.getElementById('loading').classList.remove('active');
-        });
+  const _0x1dd3b9 = _0x5a8d, _0xd75a85 = {Zrovj: function (_0xb9ec1d) {
+    return _0xb9ec1d();
+  }, zfPEN: function (_0x4ddc51, _0x16b30c) {
+    return _0x4ddc51(_0x16b30c);
+  }, nkgWN: _0x1dd3b9(486), XweyH: _0x1dd3b9(440), tTxhe: _0x1dd3b9(482), fFsti: function (_0x16f69e, _0x22ce9f) {
+    return _0x16f69e(_0x22ce9f);
+  }}, _0x2f7cce = document[_0x1dd3b9(542) + _0x1dd3b9(528)](_0xd75a85[_0x1dd3b9(553)])[_0x1dd3b9(551)];
+  selectedDate = _0x2f7cce;
+  const _0x440711 = _0x1dd3b9(527) + _0x1dd3b9(436) + _0x1dd3b9(502) + _0x1dd3b9(526) + _0x2f7cce + (_0x1dd3b9(428) + _0x1dd3b9(513) + "ta");
+  document[_0x1dd3b9(542) + _0x1dd3b9(528)](_0xd75a85[_0x1dd3b9(471)])[_0x1dd3b9(555)][_0x1dd3b9(384)](_0xd75a85[_0x1dd3b9(453)]), _0xd75a85[_0x1dd3b9(509)](fetch, _0x440711)[_0x1dd3b9(427)](_0x2af4ce => _0x2af4ce[_0x1dd3b9(452)]())[_0x1dd3b9(427)](_0x8cb4e => {
+    const _0x108332 = _0x1dd3b9;
+    allMatches = _0x8cb4e[_0x108332(549)], _0xd75a85[_0x108332(393)](populateCompetitionFilter), _0xd75a85[_0x108332(531)](displayMatches, allMatches);
+  })[_0x1dd3b9(448)](_0x1f992a => console[_0x1dd3b9(511)](_0x1dd3b9(523) + _0x1dd3b9(407), _0x1f992a))[_0x1dd3b9(535)](() => {
+    const _0xf580dd = _0x1dd3b9;
+    document[_0xf580dd(542) + _0xf580dd(528)](_0xd75a85[_0xf580dd(471)])[_0xf580dd(555)][_0xf580dd(442)](_0xd75a85[_0xf580dd(453)]);
+  });
 }
-
 function populateCompetitionFilter() {
-    const competitionFilter = document.getElementById('competitionFilter');
-    competitionFilter.innerHTML = '<option value="">Semua Kompetisi</option>';
-    let competitions = new Set(allMatches.map(match => match.competition.name));
-    competitions.forEach(competition => {
-        const option = document.createElement('option');
-        option.value = competition;
-        option.textContent = competition;
-        competitionFilter.appendChild(option);
-    });
+  const _0x34fc7c = _0x5a8d, _0x210580 = {erGhr: _0x34fc7c(512), jcEhR: _0x34fc7c(544) + _0x34fc7c(445), Alcmj: _0x34fc7c(402) + _0x34fc7c(548) + _0x34fc7c(522) + _0x34fc7c(463) + ">"}, _0xed09b1 = document[_0x34fc7c(542) + _0x34fc7c(528)](_0x210580[_0x34fc7c(473)]);
+  _0xed09b1[_0x34fc7c(419)] = _0x210580[_0x34fc7c(458)];
+  let _0xe72f9b = new Set(allMatches[_0x34fc7c(408)](_0x58f340 => _0x58f340[_0x34fc7c(544) + "n"][_0x34fc7c(459)]));
+  _0xe72f9b[_0x34fc7c(403)](_0x23cf57 => {
+    const _0x3c0e37 = _0x34fc7c, _0x5008a3 = document[_0x3c0e37(424) + _0x3c0e37(385)](_0x210580[_0x3c0e37(520)]);
+    _0x5008a3[_0x3c0e37(551)] = _0x23cf57, _0x5008a3[_0x3c0e37(494) + "t"] = _0x23cf57, _0xed09b1[_0x3c0e37(472) + "d"](_0x5008a3);
+  });
 }
-
-function displayMatches(matches) {
-    const scheduleTable = document.getElementById('schedule').getElementsByTagName('tbody')[0];
-    scheduleTable.innerHTML = '';
-
-    matches.forEach(match => {
-        const row = scheduleTable.insertRow();
-        const timeCell = row.insertCell(0);
-        const homeTeamCell = row.insertCell(1);
-        const awayTeamCell = row.insertCell(2);
-        const competitionCell = row.insertCell(3);
-
-        const matchTime = new Date(match.match_time * 1000).toLocaleTimeString('id-ID', {
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: false
-        });
-        // Mengubah format waktu dari "20.30" menjadi "20:30"
-        const formattedTime = matchTime.replace('.', ':');
-        timeCell.textContent = formattedTime;
-        homeTeamCell.innerHTML = `<img src="${match.home_team.logo}" width="20" class="me-2"> ${match.home_team.name}`;
-        awayTeamCell.innerHTML = `<img src="${match.away_team.logo}" width="20" class="me-2"> ${match.away_team.name}`;
-        competitionCell.innerHTML = `<img src="${match.competition.logo}" width="20" class="me-2"> ${match.competition.name}`;
-    });
-
-    displayMatchInfo(matches);
+function displayMatches(_0x1bdd0c) {
+  const _0x1fe491 = _0x5a8d, _0x44062f = {EjWLN: function (_0x7dcde0, _0x23c46c) {
+    return _0x7dcde0 * _0x23c46c;
+  }, XjtMq: _0x1fe491(422), cBWUJ: _0x1fe491(438), kwtNY: _0x1fe491(387), CBZbu: _0x1fe491(500), iFFgg: function (_0x180332, _0x1e4008) {
+    return _0x180332(_0x1e4008);
+  }}, _0x57a8fe = document[_0x1fe491(542) + _0x1fe491(528)](_0x44062f[_0x1fe491(516)])[_0x1fe491(542) + _0x1fe491(504)](_0x44062f[_0x1fe491(533)])[0];
+  _0x57a8fe[_0x1fe491(419)] = "", _0x1bdd0c[_0x1fe491(403)](_0x4288a5 => {
+    const _0x491795 = _0x1fe491, _0x263675 = _0x57a8fe[_0x491795(430)](), _0x1ec9c1 = _0x263675[_0x491795(418)](0), _0x4d41be = _0x263675[_0x491795(418)](1), _0x1d7655 = _0x263675[_0x491795(418)](2), _0x995537 = _0x263675[_0x491795(418)](3), _0x43c50a = new Date(_0x44062f[_0x491795(530)](_0x4288a5[_0x491795(388)], 1e3))[_0x491795(456) + _0x491795(501)](_0x44062f[_0x491795(446)], {hour: _0x44062f[_0x491795(464)], minute: _0x44062f[_0x491795(464)], hour12: false}), _0x1640d8 = _0x43c50a[_0x491795(420)](".", ":");
+    _0x1ec9c1[_0x491795(494) + "t"] = _0x1640d8, _0x4d41be[_0x491795(419)] = _0x491795(465) + _0x4288a5[_0x491795(443)][_0x491795(493)] + (_0x491795(488) + _0x491795(554) + _0x491795(470)) + _0x4288a5[_0x491795(443)][_0x491795(459)], _0x1d7655[_0x491795(419)] = _0x491795(465) + _0x4288a5[_0x491795(492)][_0x491795(493)] + (_0x491795(488) + _0x491795(554) + _0x491795(470)) + _0x4288a5[_0x491795(492)][_0x491795(459)], _0x995537[_0x491795(419)] = _0x491795(465) + _0x4288a5[_0x491795(544) + "n"][_0x491795(493)] + (_0x491795(488) + _0x491795(554) + _0x491795(470)) + _0x4288a5[_0x491795(544) + "n"][_0x491795(459)];
+  }), _0x44062f[_0x1fe491(541)](displayMatchInfo, _0x1bdd0c);
 }
-
 function filterMatches() {
-    const searchValue = document.getElementById('searchBox').value.toLowerCase();
-    const selectedCompetition = document.getElementById('competitionFilter').value;
-
-    const filteredMatches = allMatches.filter(match => {
-        const homeTeam = match.home_team.name.toLowerCase();
-        const awayTeam = match.away_team.name.toLowerCase();
-        const competition = match.competition.name;
-
-        const matchesSearch = homeTeam.includes(searchValue) || awayTeam.includes(searchValue) || competition.toLowerCase().includes(searchValue);
-        const matchesCompetition = selectedCompetition === "" || competition === selectedCompetition;
-
-        return matchesSearch && matchesCompetition;
-    });
-
-    displayMatches(filteredMatches);
+  const _0x240eb7 = _0x5a8d, _0x5e1589 = {qYrGT: function (_0x1644f7, _0x3b226e) {
+    return _0x1644f7 === _0x3b226e;
+  }, vdqAs: function (_0x4c5427, _0x59ceea) {
+    return _0x4c5427 === _0x59ceea;
+  }, TrclP: function (_0x1bdc24, _0xb8bf53) {
+    return _0x1bdc24 && _0xb8bf53;
+  }, mTHgQ: _0x240eb7(432), tTcDz: _0x240eb7(544) + _0x240eb7(445), IOnmj: function (_0x2b9716, _0x3264b7) {
+    return _0x2b9716(_0x3264b7);
+  }}, _0x23a9b9 = document[_0x240eb7(542) + _0x240eb7(528)](_0x5e1589[_0x240eb7(546)])[_0x240eb7(551)][_0x240eb7(490) + "e"](), _0xd5d604 = document[_0x240eb7(542) + _0x240eb7(528)](_0x5e1589[_0x240eb7(394)])[_0x240eb7(551)], _0x3da847 = allMatches[_0x240eb7(450)](_0x3d670c => {
+    const _0x3126b0 = _0x240eb7, _0x424e45 = _0x3d670c[_0x3126b0(443)][_0x3126b0(459)][_0x3126b0(490) + "e"](), _0x5c49a7 = _0x3d670c[_0x3126b0(492)][_0x3126b0(459)][_0x3126b0(490) + "e"](), _0x48b54b = _0x3d670c[_0x3126b0(544) + "n"][_0x3126b0(459)], _0x451d6b = _0x424e45[_0x3126b0(552)](_0x23a9b9) || _0x5c49a7[_0x3126b0(552)](_0x23a9b9) || _0x48b54b[_0x3126b0(490) + "e"]()[_0x3126b0(552)](_0x23a9b9), _0x348561 = _0x5e1589[_0x3126b0(519)](_0xd5d604, "") || _0x5e1589[_0x3126b0(524)](_0x48b54b, _0xd5d604);
+    return _0x5e1589[_0x3126b0(392)](_0x451d6b, _0x348561);
+  });
+  _0x5e1589[_0x240eb7(390)](displayMatches, _0x3da847);
 }
-
-function displayMatchInfo(matches) {
-    const matchInfoContainer = document.getElementById('matchInfoContainer');
-    matchInfoContainer.innerHTML = '';
-
-    if (matches.length === 0) {
-        matchInfoContainer.innerHTML = '<p>Tidak ada pertandingan yang cocok.</p>';
-        return;
-    }
-
-    let allMatchJson = [];
-
-    matches.forEach(match => {
-        const matchTime = new Date(match.match_time * 1000).toLocaleTimeString('id-ID', {
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: false
-        });
-        // Mengubah format waktu dari "20.30" menjadi "20:30"
-        const formattedTime = matchTime.replace('.', ':');
-        const matchDate = selectedDate; // Menggunakan tanggal yang dipilih
-
-        const matchJson = {
-            title: `${match.home_team.name} vs ${match.away_team.name}`,
-            league: match.competition.name,
-            start_time: `${matchDate}T${formattedTime}:00`,
-            duration: 110, // Placeholder untuk durasi
-            thumbnail: "00000", // Placeholder untuk thumbnail
-            watch_url: "00000" // Placeholder untuk watch_url
-        };
-        allMatchJson.push(matchJson);
-
-        const matchInfoDiv = document.createElement('div');
-        matchInfoDiv.innerHTML = `
-<p>${match.home_team.name} vs ${match.away_team.name}</p>
-<p>Kompetisi: ${match.competition.name}</p>
-<p>Tanggal: ${matchDate}</p>
-<p>Waktu: ${formattedTime}</p>
-<p>Link: <a href="https://bit.ly/zonasportlive" target="_blank">https://bit.ly/zonasportlive</a></p>
-<hr>
-`;
-        matchInfoContainer.appendChild(matchInfoDiv);
-    });
-
-    const jsonInfoDiv = document.createElement('div');
-    jsonInfoDiv.innerHTML = `
-<pre>${JSON.stringify(allMatchJson, null, 2)}</pre>
-`;
-    matchInfoContainer.appendChild(jsonInfoDiv);
-
-    document.getElementById('copyTextButton').onclick = () => copyMatchInfo(allMatchJson.map(match => `${match.title}\nTanggal: ${match.start_time.split('T')[0]}\nWaktu: ${match.start_time.split('T')[1]}\nKompetisi: ${match.league}\nLink: https://bit.ly/zonasportlive`).join('\n\n'));
-    document.getElementById('copyJsonButton').onclick = () => copyMatchInfo(JSON.stringify(allMatchJson, null, 2));
+function _0x5a8d(_0x328667, _0x4eff1e) {
+  const _0x3500f2 = _0x1e02();
+  return _0x5a8d = function (_0x563fc7, _0x2ce878) {
+    _0x563fc7 = _0x563fc7 - 377;
+    let _0x190d51 = _0x3500f2[_0x563fc7];
+    return _0x190d51;
+  }, _0x5a8d(_0x328667, _0x4eff1e);
 }
-
-function copyMatchInfo(text) {
-    navigator.clipboard.writeText(text).then(() => {
-        alert('Informasi telah disalin!');
-    }).catch(err => {
-        console.error('Tidak dapat menyalin teks: ', err);
-    });
+function displayMatchInfo(_0xa8dcad) {
+  const _0x508ef5 = _0x5a8d, _0x3a2264 = {vobeo: function (_0x56772b, _0x5e49fc) {
+    return _0x56772b * _0x5e49fc;
+  }, lKUXX: _0x508ef5(422), whRJQ: _0x508ef5(438), GLFWT: _0x508ef5(454), ivQhj: _0x508ef5(478), LGHmS: _0x508ef5(441) + _0x508ef5(481), QOVCg: function (_0x22838d, _0xc60301) {
+    return _0x22838d === _0xc60301;
+  }, wTFdt: _0x508ef5(536) + _0x508ef5(506) + _0x508ef5(525) + _0x508ef5(426) + ">", FScYm: _0x508ef5(414) + _0x508ef5(410), OPUeF: _0x508ef5(401) + _0x508ef5(410)}, _0x325910 = document[_0x508ef5(542) + _0x508ef5(528)](_0x3a2264[_0x508ef5(556)]);
+  _0x325910[_0x508ef5(419)] = "";
+  if (_0x3a2264[_0x508ef5(380)](_0xa8dcad[_0x508ef5(545)], 0)) {
+    _0x325910[_0x508ef5(419)] = _0x3a2264[_0x508ef5(480)];
+    return;
+  }
+  let _0x51bd8f = [];
+  _0xa8dcad[_0x508ef5(403)](_0x120c45 => {
+    const _0x46be04 = _0x508ef5, _0x12d01a = new Date(_0x3a2264[_0x46be04(461)](_0x120c45[_0x46be04(388)], 1e3))[_0x46be04(456) + _0x46be04(501)](_0x3a2264[_0x46be04(468)], {hour: _0x3a2264[_0x46be04(477)], minute: _0x3a2264[_0x46be04(477)], hour12: false}), _0x5e8dfd = _0x12d01a[_0x46be04(420)](".", ":"), _0xf93f67 = selectedDate, _0x6f9c60 = {title: _0x120c45[_0x46be04(443)][_0x46be04(459)] + _0x46be04(538) + _0x120c45[_0x46be04(492)][_0x46be04(459)], league: _0x120c45[_0x46be04(544) + "n"][_0x46be04(459)], start_time: _0xf93f67 + "T" + _0x5e8dfd + _0x46be04(396), duration: 110, thumbnail: _0x3a2264[_0x46be04(496)], watch_url: _0x3a2264[_0x46be04(496)]};
+    _0x51bd8f[_0x46be04(534)](_0x6f9c60);
+    const _0xd599f9 = document[_0x46be04(424) + _0x46be04(385)](_0x3a2264[_0x46be04(487)]);
+    _0xd599f9[_0x46be04(419)] = _0x46be04(515) + _0x120c45[_0x46be04(443)][_0x46be04(459)] + _0x46be04(538) + _0x120c45[_0x46be04(492)][_0x46be04(459)] + (_0x46be04(398) + _0x46be04(382)) + _0x120c45[_0x46be04(544) + "n"][_0x46be04(459)] + (_0x46be04(499) + _0x46be04(497)) + _0xf93f67 + (_0x46be04(425) + _0x46be04(514)) + _0x5e8dfd + (_0x46be04(383) + _0x46be04(529) + _0x46be04(517) + _0x46be04(483) + _0x46be04(543) + _0x46be04(415) + _0x46be04(457) + _0x46be04(412) + _0x46be04(439) + _0x46be04(521) + _0x46be04(400) + "\n"), _0x325910[_0x46be04(472) + "d"](_0xd599f9);
+  });
+  const _0x355f13 = document[_0x508ef5(424) + _0x508ef5(385)](_0x3a2264[_0x508ef5(487)]);
+  _0x355f13[_0x508ef5(419)] = _0x508ef5(378) + JSON[_0x508ef5(460)](_0x51bd8f, null, 2) + _0x508ef5(411), _0x325910[_0x508ef5(472) + "d"](_0x355f13), document[_0x508ef5(542) + _0x508ef5(528)](_0x3a2264[_0x508ef5(469)])[_0x508ef5(449)] = () => copyMatchInfo(_0x51bd8f[_0x508ef5(408)](_0xc6081f => _0xc6081f[_0x508ef5(395)] + _0x508ef5(484) + _0xc6081f[_0x508ef5(416)][_0x508ef5(485)]("T")[0] + _0x508ef5(495) + _0xc6081f[_0x508ef5(416)][_0x508ef5(485)]("T")[1] + (_0x508ef5(435) + ": ") + _0xc6081f[_0x508ef5(381)] + (_0x508ef5(475) + _0x508ef5(466) + _0x508ef5(417) + _0x508ef5(391)))[_0x508ef5(489)]("\n\n")), document[_0x508ef5(542) + _0x508ef5(528)](_0x3a2264[_0x508ef5(405)])[_0x508ef5(449)] = () => copyMatchInfo(JSON[_0x508ef5(460)](_0x51bd8f, null, 2));
 }
-
+function copyMatchInfo(_0x3b965f) {
+  const _0x42cfeb = _0x5a8d, _0x101631 = {stPRl: function (_0x20c122, _0x2fcebc) {
+    return _0x20c122(_0x2fcebc);
+  }, msDYa: _0x42cfeb(437) + _0x42cfeb(539) + _0x42cfeb(540), NdHPg: _0x42cfeb(397) + _0x42cfeb(479) + _0x42cfeb(507)};
+  navigator[_0x42cfeb(404)][_0x42cfeb(462)](_0x3b965f)[_0x42cfeb(427)](() => {
+    const _0x24b3d4 = _0x42cfeb;
+    _0x101631[_0x24b3d4(532)](alert, _0x101631[_0x24b3d4(503)]);
+  })[_0x42cfeb(448)](_0x341c39 => {
+    const _0x2abce4 = _0x42cfeb;
+    console[_0x2abce4(511)](_0x101631[_0x2abce4(474)], _0x341c39);
+  });
+}
 function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+  const _0x16f8c2 = _0x5a8d, _0x32d6b4 = {nhUXk: _0x16f8c2(491)};
+  window[_0x16f8c2(421)]({top: 0, behavior: _0x32d6b4[_0x16f8c2(386)]});
 }
-
 function handleScroll() {
-    const scrollToTopButton = document.getElementById('scrollToTopButton');
-    if (window.scrollY > 200) {
-        scrollToTopButton.style.display = 'block';
-    } else {
-        scrollToTopButton.style.display = 'none';
-    }
+  const _0x4d49be = _0x5a8d, _0x563dd1 = {loRdu: _0x4d49be(399) + _0x4d49be(550), QAoOC: function (_0x406099, _0x1c84b2) {
+    return _0x406099 > _0x1c84b2;
+  }, Xgktf: _0x4d49be(406), rFLyR: _0x4d49be(409)}, _0x2e94c1 = document[_0x4d49be(542) + _0x4d49be(528)](_0x563dd1[_0x4d49be(429)]);
+  _0x563dd1[_0x4d49be(547)](window[_0x4d49be(431)], 200) ? _0x2e94c1[_0x4d49be(508)][_0x4d49be(498)] = _0x563dd1[_0x4d49be(518)] : _0x2e94c1[_0x4d49be(508)][_0x4d49be(498)] = _0x563dd1[_0x4d49be(455)];
 }
-
-document.addEventListener('DOMContentLoaded', fetchMatches);
-window.addEventListener('scroll', handleScroll);
+document[_0x377c11(377) + _0x377c11(434)](_0x377c11(379) + _0x377c11(451), fetchMatches), window[_0x377c11(377) + _0x377c11(434)](_0x377c11(476), handleScroll);
